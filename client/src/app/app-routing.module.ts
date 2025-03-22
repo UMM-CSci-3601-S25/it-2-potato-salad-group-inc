3f51251ec70c110ad5657f358dcd4d6003aa0668
daibeemachine <ols00559@morris.umn.edu>
2025-03-22 15:51:39 -0500

    Added the join lobby function and game/lobby page from jej
    Co-authored-by: James Swearingen <swear051@morris.umn.edu>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CompanyListComponent } from './company-list/company-list.component';
 import { JoinComponent } from './join/join.component';
 import { HostComponent } from './host/host.component';
 import { AddLobbyComponent } from './host/add-lobby.component';
+import { GameComponent } from './game-page/game-page';
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
 // If 'users/:id' came first, it would accidentally catch requests to
@@ -20,7 +21,8 @@ const routes: Routes = [
   {path: 'companies', component: CompanyListComponent, title: 'Companies'},
   {path: 'join', component: JoinComponent, title: 'Join'},
   {path: 'host', component: HostComponent, title: 'Host'},
-  {path: 'host/new', component: AddLobbyComponent, title: 'Create Lobby'}
+  {path: 'host/new', component: AddLobbyComponent, title: 'Create Lobby'},
+  {path: 'game/:id', component: GameComponent, title: 'Game'}
 ];
 
 @NgModule({
--- client/src/app/join/join.component.ts
+++ client/src/app/join/join.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { Router} from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-join-component',
   templateUrl: 'join.component.html',
   styleUrls: ['./join.component.scss'],
-  imports: [MatCardModule]
+  imports: [MatCardModule, RouterLink, MatInputModule, MatFormFieldModule, MatSelectModule, FormsModule]
 })
 
 
 export class JoinComponent {
   constructor(private router: Router) {}
+
+  joinId = "";
 
 }
