86411a320333d5e775c70e99ae0a3c62bed2ad42
Mackenzie Amoako <amoak017@umn.edu>
2025-03-22 14:58:59 -0500

    displayed Lobbies
    got the lobbies to display in the join page

--- client/src/app/join/join.component.ts
+++ client/src/app/join/join.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Router} from '@angular/router';
+import { LobbyService } from '../host/lobby.service';
+import { Lobby } from '../host/lobby';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-join-component',
   templateUrl: 'join.component.html',
   styleUrls: ['./join.component.scss'],
-  imports: [MatCardModule]
+  imports: [MatCardModule, NgFor, NgIf]
 })
 
 
-export class JoinComponent {
-  constructor(private router: Router) {}
+export class JoinComponent implements OnInit {
+  lobbies: Lobby[] = [];
 
+  constructor(private router: Router, private lobbyService: LobbyService) {}
+
+  ngOnInit(): void {
+    this.lobbyService.getLobbies().subscribe(lobbies => {
+      this.lobbies = lobbies;
+    });
+  }
 }
